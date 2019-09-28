9b4bbe4ef39d1bda2d2315a63df5761788af4822
Kane Tian <kxt3@cornell.edu>
2019-09-28 00:11:27 -0400

    m

--- P1/DATASET/DATASET/train/generate_lm.py
+++ P1/DATASET/DATASET/train/generate_lm.py
@@ -74,35 +74,45 @@ decUn, decBig = generate_un_and_big('deceptive.txt')
 trUn, trBig = generate_un_and_big('truthful.txt')
 
 
-import pandas as pd
-df = pd.DataFrame(columns=['smooth', 'l1', 'truth_acc', 'dec_acc'])
-#for s in range(1,4):
-for i in range(6):
-    # truth_acc = 0
-    # dec_acc = 0
-    smooth = 0.1#float(s)/10.0
-    l1 = 0.95 + float(i)/100.0
-    l2 = 1-l1
-    row = {}
-    row['smooth'] = smooth
-    row['l1'] = l1
-    with open('../validation/truthful.txt') as file:
-        acc = 0    
-        for line in file:
-            if perplexity(line, decUn, decBig, smooth, l1, l2) > perplexity(line, trUn, trBig, smooth, l1, l2):
-                acc+=1
-        row['truth_acc'] = float(acc)/128.0
-    with open('../validation/deceptive.txt') as file:
-        acc = 0
-        for line in file:
-            if perplexity(line, decUn, decBig, smooth, l1, l2) < perplexity(line, trUn, trBig, smooth, l1, l2):
-                acc+=1
-        row['dec_acc'] = float(acc)/128.0
-    df = df.append(row, ignore_index=True)
-    print('smooth:{}, l1:{}, deceptive:{}%, truthful:{}%'.format(smooth, l1, row['dec_acc'], row['truth_acc']))
-# df.to_csv('results.csv')
-# print(perplexity('../validation/deceptive.txt', decUn, decBig, 1))
-# print(perplexity('../validation/deceptive.txt', trUn, trBig, 1))
-# print(perplexity('../validation/truthful.txt', decUn, decBig, 1))
-# print(perplexity('../validation/truthful.txt', trUn, trBig, 1))
+#import pandas as pd
+#df = pd.DataFrame(columns=['smooth', 'l1', 'truth_acc', 'dec_acc'])
+##for s in range(1,4):
+#for i in range(6):
+#    # truth_acc = 0
+#    # dec_acc = 0
+#    smooth = 0.1#float(s)/10.0
+#    l1 = 0.95 + float(i)/100.0
+#    l2 = 1-l1
+#    row = {}
+#    row['smooth'] = smooth
+#    row['l1'] = l1
+#    with open('../validation/truthful.txt') as file:
+#        acc = 0    
+#        for line in file:
+#            if perplexity(line, decUn, decBig, smooth, l1, l2) > perplexity(line, trUn, trBig, smooth, l1, l2):
+#                acc+=1
+#        row['truth_acc'] = float(acc)/128.0
+#    with open('../validation/deceptive.txt') as file:
+#        acc = 0
+#        for line in file:
+#            if perplexity(line, decUn, decBig, smooth, l1, l2) < perplexity(line, trUn, trBig, smooth, l1, l2):
+#                acc+=1
+#        row['dec_acc'] = float(acc)/128.0
+#    df = df.append(row, ignore_index=True)
+#    print('smooth:{}, l1:{}, deceptive:{}%, truthful:{}%'.format(smooth, l1, row['dec_acc'], row['truth_acc']))
 
+def generate_results(decUn, decBig, trUn, trBig):
+    import pandas as pd
+    df = pd.DataFrame(columns=['Id', 'Prediction'])
+    with open('../test/test.txt') as file:
+        for i, line in enumerate(file):
+            row = {}
+            row['Id'] = i
+            if perplexity(line, decUn, decBig, 0.1, 0.99, 0.01) > perplexity(line, trUn, trBig, 0.1, 0.99, 0.01):
+                row['Prediction'] = 0
+            else:
+                row['Prediction'] = 1
+            df = df.append(row, ignore_index = True)
+    df.to_csv('preds_2.csv')
+
+generate_results(decUn, decBig, trUn, trBig)
