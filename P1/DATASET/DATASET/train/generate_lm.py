8d481f158bdfb914fb2e8a69f8cf3c6cbc506dbe
pdarbha <darbhapranav@gmail.com>
2019-09-27 22:27:50 -0400

    hype testing

--- P1/DATASET/DATASET/train/generate_lm.py
+++ P1/DATASET/DATASET/train/generate_lm.py
@@ -1,5 +1,4 @@
 import math
-import time
 
 def generate_un_and_big(text):
     un = {}
@@ -20,6 +19,25 @@ def generate_un_and_big(text):
                         big[s] += 1
                     else:
                         big[s] = 1
+    return un, big
+
+def generate_un_and_big_from_line(line):
+    un = {}
+    big = {}
+    tokens = line.split(" ")
+    for i, token in enumerate(tokens):
+        if token in un:
+            un[token] += 1
+        else:
+            un[token] = 1
+        if i == 0:
+            big['<s> '+token] = 1
+        if i < len(tokens) - 1:
+            s = token + " " + tokens[i+1]
+            if s in big:
+                big[s] += 1
+            else:
+                big[s] = 1
     return un, big
 
 def prob_unigram(grams, unigram, smooth=0):
@@ -43,33 +61,48 @@ def prob_interp(unigrams, bigrams, phrase, smooth = 0, l1 = 0, l2 = 1):
 
 def perplexity(corpus, uni, bi, smooth = 0, l1 = 0, l2 = 1):
     
-    t = time.time()
-    uni_test, bi_test = generate_un_and_big(corpus)
-    print('time for file to be parsed: ' + str(time.time() - t))
+    bi_test = generate_un_and_big_from_line(corpus)[1]
 
     add = 0
-    x = time.time()
     for bigram in bi_test:
         add += -math.log(prob_interp(uni, bi, bigram, smooth, l1, l2))
-    print('len of bitest: ' + str(len(bi_test)))
     perp = math.exp(add / len(bi_test.keys()))
-    print('time for loop: ' + str(time.time() - x))
     return perp
 
 
 decUn, decBig = generate_un_and_big('deceptive.txt')
 trUn, trBig = generate_un_and_big('truthful.txt')
-print(sorted(decUn.items(), key = lambda x: x[1])[-10:])
-print(sorted(decBig.items(), key = lambda x: x[1])[-10:])
 
-print(sorted(trUn.items(), key = lambda x: x[1])[-10:])
-print(sorted(trBig.items(), key = lambda x: x[1])[-10:])
 
-print(prob_unigram(decUn, 'the'))
-print(prob_bigram(decUn, decBig, 'and I'))
+import pandas as pd
+df = pd.DataFrame(columns=['smooth', 'l1', 'truth_acc', 'dec_acc'])
+#for s in range(1,4):
+for i in range(6):
+    # truth_acc = 0
+    # dec_acc = 0
+    smooth = 0.1#float(s)/10.0
+    l1 = 0.95 + float(i)/100.0
+    l2 = 1-l1
+    row = {}
+    row['smooth'] = smooth
+    row['l1'] = l1
+    with open('../validation/truthful.txt') as file:
+        acc = 0    
+        for line in file:
+            if perplexity(line, decUn, decBig, smooth, l1, l2) > perplexity(line, trUn, trBig, smooth, l1, l2):
+                acc+=1
+        row['truth_acc'] = float(acc)/128.0
+    with open('../validation/deceptive.txt') as file:
+        acc = 0
+        for line in file:
+            if perplexity(line, decUn, decBig, smooth, l1, l2) < perplexity(line, trUn, trBig, smooth, l1, l2):
+                acc+=1
+        row['dec_acc'] = float(acc)/128.0
+    df = df.append(row, ignore_index=True)
+    print('smooth:{}, l1:{}, deceptive:{}%, truthful:{}%'.format(smooth, l1, row['dec_acc'], row['truth_acc']))
+# df.to_csv('results.csv')
+# print(perplexity('../validation/deceptive.txt', decUn, decBig, 1))
+# print(perplexity('../validation/deceptive.txt', trUn, trBig, 1))
+# print(perplexity('../validation/truthful.txt', decUn, decBig, 1))
+# print(perplexity('../validation/truthful.txt', trUn, trBig, 1))
 
-#print(perplexity('../validation/deceptive.txt', decUn, decBig, 1))
-#print(perplexity('../validation/deceptive.txt', trUn, trBig, 1))
-
-print(prob_unigram(decUn, 'I'))
-print(prob_interp(decUn, decBig, 'and I', 0, 0.5, 0.5))
